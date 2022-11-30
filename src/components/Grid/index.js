7a5e0d6210d7497fe33ce6f43aa154d3e8553728
MateuscardosoRocha <cardosorocha27@hotmail.com>
2022-11-30 20:03:37 -0300

    Mudanças...

---
+++ src/components/Grid/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import GridItem from '../gridItem/index'
+import * as C from './styles'
+
+
+
+
+const Grid = ({items, setItems}) => {
+
+  const onDelete = (ID) =>{
+    const newArray = items.filter((transaction) => transaction.id !== ID);
+    setItems(newArray);
+    localStorage.setItem("transaction", JSON.stringify(newArray))
+  }
+  return (
+   <C.Table>
+    <C.Thead>
+        <C.Tr>
+            <C.Th width={40}>Descrição</C.Th>
+            <C.Th width={40}>Valor</C.Th>
+            <C.Th width={10} alignCenter>Tipo</C.Th>
+            <C.Th width={10}></C.Th>
+        </C.Tr>
+    </C.Thead>
+    <C.Tbody>
+    {items?.map((item, index) => (
+          <GridItem key={index} item={item} onDelete={onDelete} />
+        ))}
+    </C.Tbody>
+   </C.Table>
+  )
+}
+
+export default Grid
---
+++ src/components/resumeItem/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as C from './styles'
+const resumeItem = ({title, Icon, value}) => {
+  return (
+    
+    <C.Container>
+      <C.Header><C.HeaderTitle>{title}</C.HeaderTitle>
+      <Icon/>
+      </C.Header>
+      <C.total>{value}</C.total>
+    </C.Container>
+  )
+}
+
+export default resumeItem
